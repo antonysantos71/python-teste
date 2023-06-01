fde67ea3438edcdc3940b23095f2b326c7530d20
Antony <115037549+antonysantos71@users.noreply.github.com>
2023-06-01 20:35:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("bem vindo as pergutas simples ");
+
+nome = input ("Qual seu nome ?");
+
+idade = int (input ("Qual sua idade ?"));
+
+sexo = input ("Qual seu sexo ?")
+
+print("hellou!!, seu nome é ",nome," sua idade é ",idade," e seu sexo é ",sexo)
